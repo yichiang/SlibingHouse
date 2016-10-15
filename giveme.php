7074a5d658c2241fd4f37492dbfaeb74c4bf9c63
yichiang <yi_chiang@outlook.com>
2016-10-15 12:08:58 -0700

    modify file

--- giveme.php
+++ giveme.php
@@ -1,22 +1,4 @@
 <?php
 
- $connectstr_dbhost = '';
- $connectstr_dbusername = '';
- $connectstr_dbpassword = '';
-
- foreach ($_SERVER as $key => $value) {
-   echo $key."=".$value."\n";
-       if (strpos($key, "MYSQLCONNSTR_") !== 0) {
- 				          continue;
- 	     }
-       $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
- 	     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
- 			 $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
- }
-
-echo 'DB_USERNAME:'
-echo $connectstr_dbusername;
-echo "\n";
-echo 'DB_PASSWORD:' . $connectstr_dbpassword . "\n";
-echo 'DB_HOST' . $connectstr_dbhost . "\n";
+ echo $_SERVER['MYSQLCONNSTR_localdb'];
 ?>
